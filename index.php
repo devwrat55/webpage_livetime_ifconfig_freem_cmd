6e2df44e8e81ae781e1582e9199281e08c920a42
Devwrat Nevewani <devwratneve@gmail.com>
2020-07-10 01:31:06 +0530

    done proper

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ function checkTime(i) {
     #txt {
         text-align: center;
         font-size: 32px;
-        background-color: white;
+        background-color: orangered;
         font-family: "Consolas";
         font-weight: bold;
     }
